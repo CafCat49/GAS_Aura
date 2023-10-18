e17ff59057e41dd099125055aed1385a35382f9c
Christina <chris.hapnes@gmail.com>
2023-10-18 16:15:03 +0200

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Caffeinated Cat
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
