940d8fc8882dcb9accc3ad717dcdf5065984f31f
Christina <chris.hapnes@gmail.com>
2023-10-20 22:14:27 +0200

    Input Data Config Asset

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -38,8 +38,14 @@ public:
 	FGameplayTag Attributes_Secondary_ManaRegeneration;
 	FGameplayTag Attributes_Secondary_PoisonResistance;
 	FGameplayTag Attributes_Secondary_MentalResistance;
- 
-protected:
+
+	//Input Tags
+	FGameplayTag InputTag_LMB;
+	FGameplayTag InputTag_RMB;
+	FGameplayTag InputTag_1;
+	FGameplayTag InputTag_2;
+	FGameplayTag InputTag_3;
+	FGameplayTag InputTag_4;
 
 private:
     static FAuraGameplayTags GameplayTags;
