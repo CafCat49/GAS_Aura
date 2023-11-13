48aefafb591463830e0f2e51218cb07259cca6de
Christina <chris.hapnes@gmail.com>
2023-11-13 21:19:49 +0100

    Damage Execution Calculation

---
+++ Source/Aura/Private/AbilitySystem/ExecCalc/EC_Damage.cpp
@@ -0,0 +1,22 @@
+// Copyright Caffeinated Cat
+
+
+#include "AbilitySystem/ExecCalc/EC_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UEC_Damage::UEC_Damage()
+{
+}
+
+void UEC_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	AActor* SourceAvatar = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	AActor* TargetAvatar = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+}
