6a4f57a22b979259555a44310fb865abe1266610
Christina <chris.hapnes@gmail.com>
2023-11-27 16:05:23 +0100

    Custom Gameplay Effect Context

---
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "GameplayEffectTypes.h"
+#include "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+
+public:
+
+	bool IsCriticalHit() const { return bIsCrit; }
+	bool IsBlockedHit() const { return bIsBlocked; }
+
+	void SetIsCriticalHit(bool bInIsCrit) { bIsCrit = bInIsCrit; }
+	void SetIsBlockedHit(bool bInIsBlocked) { bIsBlocked = bInIsBlocked; }
+	
+	virtual UScriptStruct* GetScriptStruct() const override
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	virtual bool NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess) override;
+	
+protected:
+
+	UPROPERTY()
+	bool bIsBlocked = false;
+	
+	UPROPERTY()
+	bool bIsCrit = false;
+	
+};
