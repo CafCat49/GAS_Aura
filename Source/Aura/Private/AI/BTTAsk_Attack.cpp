63ba20c1ba948a48786fd9c216c8cc4c2eac0859
Christina <chris.hapnes@gmail.com>
2024-02-04 19:37:08 +0100

    Attack task

---
+++ Source/Aura/Private/AI/BTTAsk_Attack.cpp
@@ -0,0 +1,9 @@
+// Copyright Caffeinated Cat
+
+
+#include "AI/BTTAsk_Attack.h"
+
+EBTNodeResult::Type UBTTAsk_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
---
+++ Source/Aura/Public/AI/BTTAsk_Attack.h
@@ -0,0 +1,11 @@
+// Copyright Caffeinated Cat
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlueprintBase.h"
+#include "BTTAsk_Attack.generated.h"
+
+/**
+ * 
+};
