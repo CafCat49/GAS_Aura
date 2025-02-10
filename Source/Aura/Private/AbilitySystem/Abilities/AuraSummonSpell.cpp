327260a0afaf9a94571459def8d00403c70ae483
Christina <47138990+CafCat49@users.noreply.github.com>
2025-02-10 18:00:11 +0100

    Async spawn times

--- Source/Aura/Private/AbilitySystem/Abilities/AuraSummonSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonSpell.cpp
@@ -3,7 +3,6 @@
 
 #include "AbilitySystem/Abilities/AuraSummonSpell.h"
 
-#include "Kismet/KismetSystemLibrary.h"
 
 TArray<FVector> UAuraSummonSpell::GetSpawnLocations()
 {
@@ -13,46 +12,25 @@ TArray<FVector> UAuraSummonSpell::GetSpawnLocations()
 	
 	const FVector LSpread = Fwd.RotateAngleAxis(-SpreadAngle / 2.f, FVector::UpVector);
 	TArray<FVector> SpawnLocations;
+	
 	for (int32 i = 0; i < MinionCount; i++)
 	{
 		const FVector Dir = LSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
-		const FVector SpawnPos = Pos + Dir * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+		FVector SpawnPos = Pos + Dir * FMath::FRandRange(MinSpawnDistance, MaxSpawnDistance);
+
+		FHitResult Hit;
+		GetWorld()->LineTraceSingleByChannel(
+			Hit,
+			SpawnPos + FVector(0.f, 0.f, 400.f),
+			SpawnPos - FVector(0.f, 0.f, 400.f),
+			ECC_Visibility);
+
+		if (Hit.bBlockingHit)
+		{
+			SpawnPos = Hit.ImpactPoint;
+		}
+		
 		SpawnLocations.Add(SpawnPos);
-		
-		DrawDebugSphere(
-			GetWorld(),
-			SpawnPos,
-			18.f,
-			12,
-			FColor::Blue,
-			false,
-			3.f);
-		
-		UKismetSystemLibrary::DrawDebugArrow(
-			GetAvatarActorFromActorInfo(),
-			Pos,
-			Pos + Dir * MaxSpawnDistance,
-			4.f,
-			FLinearColor::Green,
-			3.f);
-
-		DrawDebugSphere(
-			GetWorld(),
-			Pos + Dir * MinSpawnDistance,
-			5.f,
-			12,
-			FColor::Red,
-			false,
-			3.f);
-
-		DrawDebugSphere(
-			GetWorld(),
-			Pos + Dir * MaxSpawnDistance,
-			5.f,
-			12,
-			FColor::Red,
-			false,
-			3.f);
 	}
 
 	return SpawnLocations;
